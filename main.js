0cbf9dff4f0358ff8cd9061f73a6ce1e0b6a8e09
Axel Garcia <axel.garcia94@gmail.com>
2019-02-07 23:18:27 +0100

    adding landign page first version

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const main = () => {
-  console.log(`----Hello World! I hope you enjoy the site!----`);
+  console.log(`Hello World! I hope you enjoy the site!`);
 };
 
 window.addEventListener('load', main);
